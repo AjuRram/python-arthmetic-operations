c2dfbdf69bfcb958d40d06252fe330ac9174dcd3
AjuRram <90497985+AjuRram@users.noreply.github.com>
2022-10-15 22:46:28 +0530

    Add files via upload

---
+++ pyton arithmeticoperations.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value"));
+b=int(input("enter b value"));
+print("addition of a and b",a+b);
+print("subtraction of a and b",a-b);
+print("multiplication of a and b",a*b);
+print("division of a and b",a/b);
+print("remainder of a and b",a%b);
+print("exponent of and b ",a^b);
+print("floar division of a and b",a//b);
